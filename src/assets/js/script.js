c0f23a3a59ea68872d2d704f56b572ff9350c858
sifatjahan230 <44110442+sifatjahan230@users.noreply.github.com>
2020-10-30 04:38:24 -0400

    Update script.js

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -2,8 +2,12 @@
    var blur=document.getElementById('blur');
    blur.classList.toggle('active')
    document.querySelector("body").style.overflow = 'hidden';
+   document.documentElement.scrollTop;
    var popup=document.getElementById('popup');
    popup.classList.toggle('active')
+
+   document.body.scrollTop = 0;
+   document.documentElement.scrollTop = 0;
    }
    function toggle2(){
    var blur=document.getElementById('blur');
@@ -12,3 +16,7 @@
    var popup=document.getElementById('popup');
    popup.classList.toggle('active')
    }
+   function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
